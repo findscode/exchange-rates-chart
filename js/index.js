f3d649c395d36ad2f08ac5b481de16750156bace
GitStearis <egor.puisha@gmail.com>
2018-11-22 01:48:55 +0300

    Remove unused test data

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
 (function() {
-  
-
-  const data = [
-    {"currencyId":145,"date":"2018-11-10T00:00:00","rate":2.1336},
-    {"currencyId":145,"date":"2018-11-11T00:00:00","rate":2.1336},
-    {"currencyId":145,"date":"2018-11-12T00:00:00","rate":2.1336},
-    {"currencyId":145,"date":"2018-11-13T00:00:00","rate":2.1431},
-    {"currencyId":145,"date":"2018-11-14T00:00:00","rate":2.1449},
-    {"currencyId":145,"date":"2018-11-15T00:00:00","rate":2.1472},
-    {"currencyId":145,"date":"2018-11-16T00:00:00","rate":2.1222},
-    {"currencyId":145,"date":"2018-11-17T00:00:00","rate":2.1102},
-    {"currencyId":145,"date":"2018-11-18T00:00:00","rate":2.1102},
-    {"currencyId":145,"date":"2018-11-19T00:00:00","rate":2.1102},
-  ];
-
   new ApplicationService(); 
 })()
